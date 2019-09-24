cb64ca21808735ca2e2e29037eb39c12d696a3ac
Andre Cloete <andre@yoco.co.za>
2019-09-24 23:42:51 +0200

    feat: add main codebase including requirements

---
+++ geohelpers.py
@@ -0,0 +1,185 @@
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+def _check_chunksize(chunksize, df_size):
+    '''Check that the chunksize is reasonable given the size of the data'''
+    try:
+        chunksize = int(chunksize)
+    except TypeError:
+        chunksize = min(max(df_size//50,5000), 60000)
+    return chunksize
+
+def cartesian_product(*arrays):
+    '''
+    Perform a cartesian product on the provided arrays
+    arrays: numpy arrays or lists
+    '''
+    ndim = len(arrays)
+    return np.stack(np.meshgrid(*arrays), axis=-1).reshape(-1, ndim)
+
+def generate_key(df, accuracy_m=1000, lats='latitude',longs='longitude'):
+    '''
+    Generate the key based on the selected level of accuracy
+    df(dataframe): pandas-like dataframe containing the latitudes and longitudes to be converted to key
+    accuracy_m(int): desired accuracy (in meters) - should be consistent with rest of the data
+    lats(str): (optional) specify the name of the column containing the latitudes
+    longs(str): (optional) specify the name of the column containing the longitudes
+    Returns: dataframe with generated key columns attached (will replace existing 'key' column if present)
+    '''
+    print('Generating the geokey')
+    # ensure that the lats and longs correspond with the selected accuracy
+    round_level = int(5 - np.log10(accuracy_m))
+    df[lats] = df[lats].round(round_level)
+    df[longs] = df[longs].round(round_level)
+    df['geokey'] = (df[lats]*100000).astype(int).astype(str) \
+            +';'+ (df[longs]*100000).astype(int).astype(str)
+    return df
+
+def generate_points_from_coordinates(df, chunksize=None, lats='latitude',longs='longitude'):
+    '''
+    Generate a Point geometries column from the latitudes and longitudes
+    df(dataframe): pandas-like dataframe containing the latitudes and longitudes to be converted to Points
+    '''
+    print('Generating the geometry points from the coordinates')
+    chunksize = _check_chunksize(chunksize, df.shape[0])
+    print(f'selected chunksize {chunksize}', flush=True, end='')
+    dfs = []
+    for df_chunk in _chunker(df, chunksize):
+        df_ret = gpd.GeoDataFrame(df_chunk, geometry=gpd.points_from_xy(df_chunk[longs],df_chunk[lats]))
+        df_ret.crs = {'init': 'epsg:4326'}
+        dfs.append(df_ret)
+        print('.', flush=True, end='')
+    print('Done!')
+    return pd.concat(dfs)
+
+def _generate_smaller_grid(lats, longs, accuracy_m, verbose):
+    '''For use with generate grid'''
+    mlat, mlong = len(lats), len(longs)
+    if max(mlat,mlong) >= 1500:
+        # potentially too large to handle in one go...dividing and conquering
+        dfs = []
+        if mlat>mlong: 
+            for lat in np.array_split(lats,mlat//100):
+                dfs += [_generate_smaller_grid(lat,longs,accuracy_m,verbose)]
+        else:
+            for lon in np.array_split(longs,mlong//100):
+                dfs += [_generate_smaller_grid(lats,lon,accuracy_m,verbose)]
+        grid = pd.concat(dfs)
+        if verbose: print('collecting results')
+
+    else:
+        if verbose: print('.',end='',flush=True)
+        cols=['latitude','longitude']
+        grid = pd.DataFrame(cartesian_product(lats,longs), columns=cols)
+    return grid
+
+def generate_grid(lats, longs, accuracy_m=1000, verbose=False):
+    '''Create accuracy_m spaced grid using (min,max) pairs provided in lats, longs
+    longs(array-like): min,max values for longitude range
+    lats(array-like): min,max values for latitude range
+    accuracy_m(int): desired approximate accuracy in meters from (1,10,100,1000,10000,100000)
+    verbose(bool): set verbosity level
+    Returns: GeoDataFrame with latitudes, longitudes, coordinates and constructed key
+    '''
+    try:
+        accuracy_m = int(accuracy_m)
+        if not accuracy_m in (10**x for x in range(6)):
+            raise ValueError('accuracy_m not in (1,10,100,1000,10000,100000)')
+    except Exception:
+        raise ValueError('accuracy_m must be one of these values: (1,10,100,1000,10000,100000)')
+    
+    steps = accuracy_m/100000
+    lats = np.arange(np.min(lats), np.max(lats), steps)
+    longs = np.arange(np.min(longs), np.max(longs), steps)
+    print('Generating point grid')
+    ret = _generate_smaller_grid(lats,longs,accuracy_m,verbose).reset_index(drop=True)
+    print(f'\nGrid of size {ret.shape} generated!')
+    return ret
+
+def _chunker(df, chunksize=None):
+    '''
+    df(dataframe): Pandas-like df to be split into chunks
+    chunksize(int): desired size of smaller dfs
+    returns: generator of dfs with at most chunksize rows or at most chunks entries
+    '''
+    if not chunksize:
+        return [df]
+    df_in = df.copy()
+    dfs = []
+    for (g,df) in df_in.groupby(np.arange(df_in.shape[0]) // chunksize):
+        dfs+=[df]
+    return dfs
+
+def do_join(points, geometries):
+    '''
+    Helper to do the geometry join
+    '''
+    joined = gpd.sjoin(points, geometries, how='inner', op='within', rsuffix='geometries')
+    return joined
+
+def locate_points(points, geometries, chunksize=None, verbose=False):
+    '''
+    Locate the points within the provided geometries
+    points(geopandasdf): GeoPandasDF containing a single column of Points to locate in geometries
+    geometries(geopandasdf): GeoPandasDF conatining a single column of Geometries
+    chunksize(int): (optional) specify the size of the chunks in which to process the points
+    verbose(bool): Do you want all the information? (Default False)
+    '''
+    print('Locating the points')
+    results = []
+    chunksize = _check_chunksize(chunksize, points.shape[0])
+    if points.shape[0] > 500000:
+        print('There are many points to locate - this is going to take a while!')
+    n,steps = 0,0
+    for small_points in _chunker(points, chunksize):
+        if verbose: print('.',flush=True,end='')
+        small_join = do_join(small_points, geometries)
+        results += [small_join]
+        steps += 1
+        n += small_points.shape[0]
+        if steps%10==0:
+            print(f' ({n} of {points.shape[0]} [{n*100//points.shape[0]}%] processed)')
+    print('Combining results')
+    ret = pd.concat(results)
+    print(f'Done!')
+    if verbose: 
+        print(f'{ret.shape[0]} of {points.shape[0]} located within geometries')
+        print(f'{points.shape[0] - ret.shape[0]} points were not found within provided geometries!')
+    return ret.reset_index(drop=True)
+
+def save_data(df, filename='filename.json.gz', directory='processed_data', columns=None):
+    '''
+    Save the data in json records format, optionally only keeping certain columns
+    df(dataframe): dataframe to save
+    filename(str): the name of the destination filename, including extension and compression
+    directory(str): the directory in which to save the files
+    columns(list/dict): list of columns to keep in the saved file.  
+    If dictionary is passed in columns, the keys will be used to filter the df 
+    while the (key:value) pairs will be used to rename the columns
+    '''
+    if not isinstance(columns, dict):
+        if not isinstance(columns, list):
+            df = df[list(columns)]
+    else:
+        df = df[columns.keys()]
+        df = df.rename(columns=columns)
+    df = pd.DataFrame(df)
+    return df
+    try:
+        # Create target Directory
+        directory='processed_data'
+        os.mkdir(directory)
+        print(f"Directory {directory} created")
+    except FileExistsError:
+        print(f"Directory {directory} already exists")
+    full_pathname = os.path.join(directory,filename)
+    print(f'Writing file at {full_pathname}')
+    df.to_json(full_pathname,orient='records')
+
+def process_dataframe(df, geometries, accuracy_m=1000, chunksize=None, verbose=False):
+    chunksize = _check_chunksize(chunksize, df.shape[0])
+    df = generate_key(df, accuracy_m)
+    df = generate_points_from_coordinates(df, chunksize)
+    df = locate_points(df, geometries, chunksize, verbose)
+    return df
---
+++ main.py
@@ -0,0 +1,101 @@
+## ensure that the data folder is colocated with this file
+## if you don't have the data folder, run the get_data.sh script first
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import geohelpers as gh
+import os, sys
+
+def load_raw_data():
+    print('Reading geonames dataset')
+    geo_cols = ['geonameid', 'name', 'asciiname', 'alternatenames', 'latitude', 'longitude',
+                'feature_class', 'feature_code', 'country_code', 'cc2',
+                'admin1_code', 'admin2_code', 'admin3_code', 'admin4_code',
+                'population', 'elevation', 'dem', 'timezone', 'modification_date',]
+    geonames = pd.read_csv('data/geonames.tsv', sep='\t', names=geo_cols, dtype=object)
+    geonames['f_code'] = geonames.feature_class.astype(str)+'.'+(geonames.feature_code).astype(str)
+    geofeats_cols = ['f_code','desc_short','desc_long']
+    geofeats = pd.read_csv('data/geonames_features.tsv', sep='\t', names=geofeats_cols)
+    geonames = geonames.merge(geofeats, how='left',on='f_code')
+    geonames[['latitude','longitude']] = geonames[['latitude','longitude']].astype(float)
+    geonames[['geonameid','population']] = geonames[['geonameid','population']].astype(int)
+
+    print('Reading postal codes')
+    pc_cols = ['country_code', 'postal_code', 'place_name', 
+               'admin_name1', 'admin_code1', 'admin_name2', 'admin_code2', 
+               'admin_name3', 'admin_code3', 'latitude', 'longitude', 'accuracy',]
+    postal_codes = pd.read_csv('data/postal_codes.tsv', sep='\t', names=pc_cols, dtype={1:object})
+    postal_codes[['latitude','longitude']] = postal_codes[['latitude','longitude']].astype(float)
+
+    print('Reading wards')
+    geometries = gpd.read_file('data/MDBWard2016.gdb/').set_geometry('geometry')
+    
+    print('All files read!')
+    
+    return geometries, geonames, postal_codes
+
+def extract_names_datasets(located_geonames):
+    ## Descoped nearest points for now
+    ## see here on how to do it: https://gis.stackexchange.com/questions/222315/geopandas-find-nearest-point-in-other-dataframe
+    ## or here: https://stackoverflow.com/questions/56520780/how-to-use-geopanda-or-shapely-to-find-nearest-point-in-same-geodataframe
+    lg = located_geonames
+    provinces = pd.DataFrame(lg[lg.feature_code=='ADM1']).reset_index(drop=True)
+    districts = pd.DataFrame(lg[lg.feature_code=='ADM2']).reset_index(drop=True)
+    towns = pd.DataFrame(lg[lg.feature_code=='ADM3']).reset_index(drop=True)
+    suburbs = pd.DataFrame(lg[lg.feature_class=='P']).reset_index(drop=True)
+    return provinces, districts, towns, suburbs
+
+def process_data(accuracy_m=1000, verbose=False):
+    geometries, geonames, postal_codes = load_raw_data()
+    points_grid = gh.generate_grid(lats=[-35,-21], longs=[15,34], accuracy_m=accuracy_m, verbose=verbose)
+    
+    print('\nProcessing the generated grid dataset')
+    located_grid = gh.process_dataframe(points_grid, geometries, accuracy_m, verbose=verbose)
+    del points_grid # free up RAM
+    grid_cols = {
+        'WardID':'ward_id',
+        'WardNumber':'ward_number',
+        'Shape_Length':'ward_length',
+        'Shape_Area':'ward_area',
+        'LocalMunicipalityName':'local_municipality',
+        'DistrictMunicipalityCode':'district_minicipal_code',
+        'DistrictMunicipalityName':'district_municipality',
+        'ProvinceName':'province_code',
+        'ProvinceCode':'province_name',
+        'geokey':'geokey',
+        'latitude':'latitude',
+        'longitude':'longitude'}
+    gh.save_data(located_grid, 'located_grid.json.gz','processed_data',columns=grid_cols)
+    
+    print('\nProcessing the geonames dataset')
+    located_geonames = gh.process_dataframe(geonames, geometries, accuracy_m, verbose=verbose)
+    geonames_cols = {
+        'geonameid':'geoname_id',
+        'name':'name',
+        'feature_class':'feature_class',
+        'feature_code':'feature_code',
+        'population':'population',
+        'desc_short':'desc_short',
+        'desc_long':'desc_long',
+        'geokey':'geokey',
+        'WardID':'ward_id',
+        'latitude':'latitude',
+        'longitude':'longitude',
+        }
+    located_geonames = located_geonames[geonames_cols].rename(columns=geonames_cols)
+    located_geonames['desc_long'] = located_geonames['desc_long'].fillna(located_geonames['desc_short'])
+    located_geonames = located_geonames.dropna()
+    provinces, districts, towns, suburbs = extract_names_datasets(located_geonames)
+    
+    
+    print('\nProcessing the postal_codes dataset')
+    located_postal_codes = gh.process_dataframe(postal_codes, geometries, accuracy_m, verbose=verbose)
+    
+    print('Done!')
+    return (located_grid, located_geonames, located_postal_codes)
+    
+if __name__=='__main__':
+    (located_grid, located_geonames, located_postal_codes) = process_data(accuracy_m=1000, verbose=True)
+    print([x.shape for x in (located_grid, located_geonames, located_postal_codes)])
+    provinces, districts, towns, suburbs = extract_names_datasets(located_geonames)
+    print([x.shape for x in (provinces, districts, towns, suburbs)])
